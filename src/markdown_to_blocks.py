5bffddc50f630e13624c43ba21d33c95390d3963
Jack Aldus <jack.aldus@gmail.com>
2026-06-01 22:33:12 +0100

    F: added single block conversion from md text to a block type. associated unit tests added to test file.

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     # remove block separation, ie. new paragraphs "\n\n" in md text.
     markdown_split = markdown.split("\n\n")
@@ -12,3 +15,55 @@ def markdown_to_blocks(markdown):
         else:
             continue
     return striped_blocks
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(markdown_striped_block):
+    # checking an input block for a heading block, level 1 to 6.
+    if markdown_striped_block.startswith(
+        ("# ", "## ", "### ", "#### ", "##### ", "###### ")
+    ):
+        return BlockType.HEADING
+
+    # checking an input block for a code block.
+    if markdown_striped_block.startswith("```\n") and markdown_striped_block.endswith(
+        "```"
+    ):
+        return BlockType.CODE
+
+    # checking an input block for a quote block.
+    if markdown_striped_block.startswith((">", "> ")):
+        split_result = markdown_striped_block.split("\n")
+        for result in split_result:
+            if not result.startswith((">", "> ")):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    # checking an input block for an unordered list block.
+    if markdown_striped_block.startswith("- "):
+        split_result = markdown_striped_block.split("\n")
+        for result in split_result:
+            if not result.startswith(("- ")):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    # checking an input block for an ordered list block.
+    if markdown_striped_block.startswith("1. "):
+        split_result = markdown_striped_block.split("\n")
+        i = 1
+        for result in split_result:
+            if not result.startswith((f"{i}. ")):
+                return BlockType.PARAGRAPH
+            else:
+                i += 1
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,10 +1,10 @@
 import unittest
 
-from markdown_to_blocks import markdown_to_blocks
+from markdown_to_blocks import BlockType, block_to_block_type, markdown_to_blocks
 
 
-# testing basic conversion, text from lesson.
 class TestMarkdownToBlocks(unittest.TestCase):
+    # testing basic conversion, text from lesson.
     def test_markdown_to_blocks(self):
         md = (
             "This is **bolded** paragraph"
@@ -91,6 +91,53 @@ class TestMarkdownToBlocks(unittest.TestCase):
             ],
             blocks,
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    # testing overall basic functionality for converting all types starting with headings.
+    def test_block_to_block_type_headings(self):
+        block = "# this is a heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    # testing an code block.
+    def test_block_to_block_type_code(self):
+        block = "```\n this is code```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    # testing an quote block.
+    def test_block_to_block_type_quote(self):
+        block = "> this is a quote block"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    # testing an quote block with multiple lines.
+    def test_block_to_block_type_quote_multiline(self):
+        block = "> this is a quote block\n> with multiple lines"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    # testing an unordered list block.
+    def test_block_to_block_type_unordered_list(self):
+        block = "- this is an unordered list block"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    # testing an unordered list block with multiple lines.
+    def test_block_to_block_type_unordered_list_multiline(self):
+        block = "- this is an unordered list block\n- with multiple lines"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    # testing an ordered list block.
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. this is an ordered list block"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+    # testing an ordered block with multiple lines.
+    def test_block_to_block_type_ordered_list_multiline(self):
+        block = "1. this is an ordered list block\n2. with multiple lines"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+    # testing an incorrectly ordered block with multiple lines.
+    def test_block_to_block_type_incorrect_ordered_list_multiline(self):
+        block = "1. this is an unordered list block\n3. with multiple lines\n2. that is incorrect"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
